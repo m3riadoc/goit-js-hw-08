0c4d4a565a1311e48864ccc3faea4b3577158536
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2024-01-04 16:13:40 +0100

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(item => {
+  const { original, preview, description } = item;
+
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+
+  const a = document.createElement('a');
+  a.classList.add('gallery__link');
+  a.href = original;
+
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = preview;
+  img.alt = description;
+
+  a.appendChild(img);
+  li.appendChild(a);
+  gallery.appendChild(li);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionsDataAlt: 'Image description',
+  });
+});
