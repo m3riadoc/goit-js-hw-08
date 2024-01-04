19ee0f92477b996e3f5061b82936d8ceacc2bd90
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2024-01-04 18:17:35 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,8 @@
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 
@@ -35,3 +33,4 @@ document.addEventListener('DOMContentLoaded', () => {
     captionsDataAlt: 'Image description',
   });
 });
+console.log(galleryItems);
