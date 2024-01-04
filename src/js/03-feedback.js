c8738d1aeb51da8751d9becd97b6f215aefaea7a
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2024-01-04 23:47:41 +0100

    commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle'; 
 
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+form.elements.email.value = savedData.email || '';
+form.elements.message.value = savedData.message || '';
+
+
+const saveFormData = () => {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+};
+
+
+const throttledSaveFormData = throttle(saveFormData, 500);
+
+form.addEventListener('input', throttledSaveFormData);
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
